b6c94b48950a3801d36b2f0683b3f19c2c82ead4
jjodx <jj.orban@kuleuven.be>
2018-07-21 18:03:36 +0200

    Le Neurone moteur

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("C:\Users\dropbox\Documents\Dropbox (Personal)\website")
+setwd("C:/Users/dropbox/Documents/Dropbox (Personal)/website")
 
 #render your sweet site. 
 rmarkdown::render_site()
