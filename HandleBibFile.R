62373ca7824331e9abea4d670797834e7844076f
Jean-Jacques Orban de Xivry <u0099946@kuleuven.be>
2020-05-27 23:36:38 +0200

    update all files

--- HandleBibFile.R
+++ HandleBibFile.R
@@ -1,8 +1,10 @@
-
-library(bib2df)
-bib_df<-bib2df(file="MyPub.bib")
-
 library(stringi)
+library(bib2df)
+
+# bib_df<-bib2df(file="MyPub.bib")
+bib_df<-bib2df(file="ExportedItems.bib")
+
+
 
 bib_df$TITLE<-stri_replace_all_regex(bib_df$TITLE, "[\\{\\}]", "")
 bib_df$JOURNAL<-stri_replace_all_regex(bib_df$JOURNAL, "[\\{\\}]", "")
@@ -17,10 +19,10 @@ bib_df<-bib_df[order(bib_df$YEAR, decreasing=T),]
 
 dims<-dim(bib_df)
 p<-c("D:/Dropbox (INMACOSY)/Biblio/" )
-p2<-c("C:/Users/u0099946/Documents/website/PubFile/")
+p2<-c("C:/Users/u0099946/Dropbox (Personal)/website/PubFile")
 pg = c("https://github.com/jjodx/jjodx.github.io/raw/master/PubFile")
 
-for(i in 1:dims[1]){
+for(i in 1:6){#dims[1]
   # create file name from DOI
   b<-bib_df[i,]$DOI
   b<-gsub("\\.", "_", b)
@@ -28,24 +30,18 @@ for(i in 1:dims[1]){
   
   # find original file
   a<-bib_df$FILE[i]
+  babab<-gsub("\\\\\\\\","/",a)
+  a<-gsub("\\\\","",babab)
   split_a1<-unlist(strsplit(a,"/"))
   split_a<-unlist(strsplit(split_a1[length(split_a1)],":"))
-  p3<-paste0(c(p,bib_df[i,]$YEAR,"/"),collapse="")
-  bib_df$FILE[i]<-paste0(c(p3,split_a[1]),collapse="")
+  p3<-paste(c(split_a1[1:length(split_a1)-1]),collapse="/")
+  bib_df$FILE[i]<-paste0(c(p3,"/",split_a[1]),collapse="")
   
   if(file.exists(bib_df$FILE[i])){
     file.copy(from = file.path(p3,split_a[1]), to = file.path(p2, paste0(c(b,".pdf"),collapse="")),overwrite = TRUE)
   }else{
-    #### if no match was found, try to find the filename that closely ressembles the given filename
-    FileList <- list.files(p3);# list all files in teh directory where we look for the file.
-    FileNumber <- agrep(split_a[1],FileList)
-    FileName <-FileList[FileNumber]
-    if(file.exists(paste0(c(p3,FileName),collapse=""))){
-      file.copy(from = file.path(p3,FileName), to = file.path(p2, paste0(c(b,".pdf"),collapse="")),overwrite = TRUE)
-    }
-    else{
     print(i)
-    print(FileName)}
+    print(FileName)
   }
 }
 
